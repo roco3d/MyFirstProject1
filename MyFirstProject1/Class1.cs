1ab996d2d4a666549e04a066a7ff71c2e67ca27d
Cortes <rodrigo.cortes@amecfw.com>
2017-12-04 17:26:28 -0300

    Add project files.

---
+++ MyFirstProject1/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+
+[assembly: CommandClass(typeof(MyFirstProject1.Class1))]
+
+namespace MyFirstProject1
+{
+    public class Class1
+    {
+        [CommandMethod("AdskGreeting")]
+        public void AdskGreeting()
+        {
+            // Get the current document and database, and start a transaction
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Starts a new transaction with the Transaction Manager
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the BLOCK table record for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                // Open the BLOCK table record Model space for write
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+                // Creates a new MTEXT object and assigns it a location, text value and text style
+                MText objText = new MText();
+
+                // Specify the insertion point of the MTEXT object
+                objText.Location = new Autodesk.AutoCAD.Geometry.Point3d(2, 2, 0);
+
+                // Set the text string for the MTEXT object
+                objText.Contents = "Greetings, welcome to AutoCAD .NET programming";
+
+                // Set the text style
+                objText.TextStyleId = acCurDb.Textstyle;
+
+                // Append the new MTEXT to model space
+                acBlkTblRec.AppendEntity(objText);
+
+                // Append the new MTEXT object to the active transaction
+                acTrans.AddNewlyCreatedDBObject(objText, true);
+
+                // Saves the changes to database and close the transaction
+                acTrans.Commit();
+            }
+        }
+
+        [CommandMethod("ListEntities")]
+        public static void ListEntities()
+        {
+            // Get the current document and database, and start a transaction
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the BLOCK table record for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,OpenMode.ForRead) as BlockTable;
+
+                // Open the BLOCK table record Model space for read
+                BlockTableRecord acBlkTblRec;
+                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace], OpenMode.ForRead) as BlockTableRecord;
+
+                int nCnt = 0;
+                acDoc.Editor.WriteMessage("\nModel space objects: ");
+
+                // Step through each object in Model space and display the type of object found
+                foreach (ObjectId acObjId in acBlkTblRec)
+                {
+                    acDoc.Editor.WriteMessage("\n" + acObjId.ObjectClass.DxfName);
+                    nCnt = nCnt + 1;
+                }
+
+                // If no objects are found then display a message
+                if (nCnt==0)
+                {
+                    acDoc.Editor.WriteMessage("\n No objects found");
+                }
+
+                // Dispose of he transaction
+            }
+        }
+    }
+}
